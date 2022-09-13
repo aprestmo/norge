94aae1f35bbf52524cc2834ae6446eb95d0aa0cc
Alexander Prestmo <aprestmo@gmail.com>
2022-09-13 12:09:04 +0200

    Change to yarn and add some integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import image from "@astrojs/image";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    svelte(),
-  ]
+  integrations: [tailwind(), svelte(), image(), compress()]
 });
