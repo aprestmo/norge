b12bd8d463bd6bf39185dda61ae59b85c71cf682
Alexander Prestmo <aprestmo@gmail.com>
2023-09-18 09:46:32 +0200

    astro@3.1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
-import image from "@astrojs/image";
-
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), image(), compress()]
+  integrations: [tailwind(), svelte()]
 });
